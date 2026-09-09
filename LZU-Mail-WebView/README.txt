d829d30c1d9bc6ac3c8a6605c1d00bfdb4ca960e
Vale <2557981809@qq.com>
2026-09-09 11:42:42 +0000

    Add LZU Mail WebView Android app

---
+++ LZU-Mail-WebView/README.txt
@@ -0,0 +1,8 @@
+LZU Mail WebView wrapper
+Target: https://mail.lzu.edu.cn
+App name: 兰大邮箱
+Package: cn.edu.lzu.mailwebview
+
+Build in Android Studio: open this folder, let Gradle sync, then Build > Build APK(s).
+CLI: gradle :app:assembleDebug
+Expected output: app/build/outputs/apk/debug/app-debug.apk
---
+++ LZU-Mail-WebView/app/src/main/java/cn/edu/lzu/mailwebview/MainActivity.java
@@ -0,0 +1,50 @@
+package cn.edu.lzu.mailwebview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setLoadWithOverviewMode(true);
+        s.setUseWideViewPort(true);
+        s.setBuiltInZoomControls(true);
+        s.setDisplayZoomControls(false);
+
+        CookieManager cm = CookieManager.getInstance();
+        cm.setAcceptCookie(true);
+        cm.setAcceptThirdPartyCookies(webView, true);
+
+        webView.setWebViewClient(new WebViewClient());
+        if (savedInstanceState == null) {
+            webView.loadUrl("https://mail.lzu.edu.cn");
+        } else {
+            webView.restoreState(savedInstanceState);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        webView.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
