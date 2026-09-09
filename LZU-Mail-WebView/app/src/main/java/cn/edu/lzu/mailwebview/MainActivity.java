4d09096e6af1e6122c9500dcc5f50633c9f8caae
Vale <2557981809@qq.com>
2026-09-09 12:00:52 +0000

    Fix Coremail mobile WebView loading

--- LZU-Mail-WebView/app/src/main/java/cn/edu/lzu/mailwebview/MainActivity.java
+++ LZU-Mail-WebView/app/src/main/java/cn/edu/lzu/mailwebview/MainActivity.java
@@ -3,34 +3,93 @@ package cn.edu.lzu.mailwebview;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
     private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         webView = new WebView(this);
         setContentView(webView);
 
-        WebSettings s = webView.getSettings();
-        s.setJavaScriptEnabled(true);
-        s.setDomStorageEnabled(true);
-        s.setLoadWithOverviewMode(true);
-        s.setUseWideViewPort(true);
-        s.setBuiltInZoomControls(true);
-        s.setDisplayZoomControls(false);
+        WebSettings settings = webView.getSettings();
 
-        CookieManager cm = CookieManager.getInstance();
-        cm.setAcceptCookie(true);
-        cm.setAcceptThirdPartyCookies(webView, true);
+        // Coremail 页面需要
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
 
-        webView.setWebViewClient(new WebViewClient());
+        // 页面显示
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+
+        // 缩放
+        settings.setSupportZoom(true);
+        settings.setBuiltInZoomControls(true);
+        settings.setDisplayZoomControls(false);
+
+        // 让网站把我们识别成普通手机浏览器，而不是特殊 WebView
+        String ua = settings.getUserAgentString();
+        ua = ua.replace("; wv", "");
+        ua = ua.replace("Version/4.0 ", "");
+        settings.setUserAgentString(ua);
+
+        // Cookie，登录邮箱必须
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        cookieManager.setAcceptThirdPartyCookies(webView, true);
+
+        // 支持 JavaScript 对话框等网页功能
+        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(
+                    WebView view,
+                    WebResourceRequest request) {
+                return false;
+            }
+
+            @Override
+            public void onReceivedError(
+                    WebView view,
+                    WebResourceRequest request,
+                    WebResourceError error) {
+
+                if (request.isForMainFrame()) {
+                    String message =
+                            "<html><body style='font-family:sans-serif;padding:30px'>" +
+                            "<h2>兰大邮箱加载失败</h2>" +
+                            "<p>" + error.getDescription() + "</p>" +
+                            "<p>请检查网络连接后重新打开 App。</p>" +
+                            "</body></html>";
+
+                    view.loadDataWithBaseURL(
+                            null,
+                            message,
+                            "text/html",
+                            "UTF-8",
+                            null
+                    );
+                }
+            }
+        });
+
         if (savedInstanceState == null) {
-            webView.loadUrl("https://mail.lzu.edu.cn");
+            // 兰大 Coremail 手机端
+            webView.loadUrl(
+                    "https://mail.lzu.edu.cn/coremail/xphone/"
+            );
         } else {
             webView.restoreState(savedInstanceState);
         }
@@ -44,7 +103,18 @@ public class MainActivity extends Activity {
 
     @Override
     public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) webView.goBack();
-        else super.onBackPressed();
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.destroy();
+        }
+        super.onDestroy();
     }
 }
